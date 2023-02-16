3c0b0cf446f914b40c4379b38f60f4e9369068ab
schooltape <102953226+schooltape@users.noreply.github.com>
2023-02-16 17:34:16 +1100

    Adds dialogs
    a truckload of code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("script.js loaded");
+
 document.querySelector(".button-install").addEventListener("click", () => {
     window.open("https://github.com/schooltape-community/schooltape/releases/latest", "_blank");
 });
@@ -7,5 +9,26 @@ document.querySelector(".button-source").addEventListener("click", () => {
 
 // listener for when the page is loaded
 window.addEventListener("load", () => {
+    console.log("page loaded");
     document.body.classList.add("load");
+});
+
+document.querySelectorAll(".dialog-opener").forEach((button) => {
+    button.addEventListener("click", () => {
+        var buttonClass = button.classList[1];
+        var dialogElement = document.getElementsByClassName(buttonClass)[1];
+        dialogElement.hidden = false;
+        console.log(buttonClass + " dialog opened");
+    });
+});
+
+document.querySelectorAll(".dialog-dismiss").forEach((button) => {
+    console.log("button found");
+    button.addEventListener("click", () => {
+        // hides all the fluent dialogs
+        document.querySelectorAll("fluent-dialog").forEach((dialog) => {
+            dialog.hidden = true;
+        });
+        console.log("dialogs closed");
+    });
 });
