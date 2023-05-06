6753859fe65d8a515b0b66ad67acd4e6da40b943
42willow <102953226+schooltape@users.noreply.github.com>
2023-05-06 15:25:11 +1000

    Fix image animations on homepage #2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("script.js loaded");
 
+let currentAnimation = false;
 // listener for when the page is loaded
 window.addEventListener("load", () => {
     console.log("page loaded");
@@ -17,23 +18,15 @@ document.querySelector(".button-source").addEventListener("click", () => {
 for (let i = 0; i < 3; i++) {
     document.getElementsByClassName("learning-button")[i].addEventListener("click", () => {
         var image = document.getElementsByClassName("learning-image")[0];
-        // if an animation is already running, don't do anything
-        if (!image.classList.contains("animation") && !image.src.includes(document.getElementsByClassName("learning-button")[i].classList[1])) {
-            // add the class "animation" to the image
-            image.classList.add("animation");
-            // change the image
-            setTimeout(() => { // delay changing image to make animation look better
+         // fade image out and in, if an animation is not already running and the image is not already the same
+        if (!currentAnimation && !image.src.includes(document.getElementsByClassName("learning-button")[i].classList[1])) {
+            currentAnimation = true;
+            image.style.opacity = 0;
+            setTimeout(() => {
                 image.src = "assets/" + document.getElementsByClassName("learning-button")[i].classList[1] + ".jpg";
+                image.style.opacity = 1;
+                currentAnimation = false;
             }, 500);
-
-            image.style.animation = "fade-out-in 1s ease-in-out";
-            setTimeout(() => {
-                image.style = "";
-            } , 1000);
-            // remove the class "animation" from the .learning-image
-            setTimeout(() => {
-                image.classList.remove("animation");
-            } , 1000);
         }
     });
 }
