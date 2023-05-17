51a3f1aab3da893100f5336967903f6ddddd7744
42willow <70784752+42Willow@users.noreply.github.com>
2023-05-17 21:12:58 +1000

    cult ad + current ver

---
+++ assets/cult-ad.md
@@ -0,0 +1,37 @@
+# Introducing [Schooltape](https://schooltape-community.github.io/): Customise Your Schoolbox Experience!
+
+🎉 Welcome to Schooltape, the ultimate chrome extension designed to enhance your Schoolbox experience! 🌟
+
+#### It's FOSS
+
+🆓 Best of all, Schooltape is completely free and open-source! It empowers you to personalise the appearance, functionality, and feel of Schoolbox to match your preferences.
+
+#### Customisable 🔧
+
+With Schooltape, you have the power to tailor Schoolbox to your liking. From aesthetics to functionality, you can make it truly yours.
+
+#### Seamless Integration 🚀
+
+Schooltape automatically detects when you're on a Schoolbox page and activates its features accordingly. You can also use Schooltape on other websites without any interference.
+
+#### Focus on Learning 🧠
+
+Worried about Schooltape affecting your quizzes or tests? Don't be! It intelligently refrains from activating during assessments, ensuring an uninterrupted learning experience.
+
+#### Get Started 📥
+
+To begin customising your Schoolbox, head over to our GitHub repository and download the Schooltape chrome extension. Take control of your learning environment today!
+Installing Schooltape is easy.
+
+##### Just follow the steps below:
+
+1. Download the latest version of Schooltape from the [releases](https://github.com/schooltape-community/schooltape/releases/latest) page.
+2. Unzip the file.
+3. Go to chrome://extensions in your browser.
+4. Enable developer mode.
+5. Click "Load unpacked" and select the folder that you unzipped. Navigate to the /Extension directory.
+6. Enjoy!
+
+#### More info ℹ️
+
+[Join](https://schooltape-community.github.io/) the Schooltape <del>`cult`</del> revolution and make your Schoolbox experience truly unique!
--- script.js
+++ script.js
@@ -32,3 +32,59 @@ for (let i = 0; i < 3; i++) {
         }
     });
 }
+
+
+
+// RELEASE BADGE
+const owner = 'schooltape-community';
+const repo = 'schooltape';
+fetch(`https://api.github.com/repos/${owner}/${repo}/releases/latest`)
+.then(response => response.json())
+.then(data => {
+    const tagName = data.tag_name;
+    const releaseName = data.name;
+    const releaseUrl = data.html_url;
+    const badge = document.querySelector(".release-badge");
+
+    badge.innerHTML = `Latest Release: ${tagName}`;
+    // add an onclick listener
+    badge.addEventListener("click", () => {
+        window.open(releaseUrl, "_blank");
+    });
+    
+    console.log(`Latest release: ${tagName}`);
+    console.log(`Release name: ${releaseName}`);
+    console.log(`Release URL: ${releaseUrl}`);
+})
+.catch(error => {
+    console.error('Error fetching the latest release:', error);
+});
+
+
+// Define the desired dimensions
+const desiredWidth = 1045; // Replace with your desired width
+
+// Get the actual dimensions of the website
+const actualWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+// Compare the dimensions and show warning if necessary
+if (actualWidth < desiredWidth) {
+  // Show warning
+  window.alert("Your screen appears to be too thin for this website. Please resize your window otherwise some elements will be bugged (it's poorly designed sorry)");
+}
+
+
+// Cult ad listener
+const copyMarkdown = async () => {
+try {
+    const response = await fetch('https://schooltape-community.github.io/assets/cult-ad.md');
+    const markdown = await response.text();
+    navigator.clipboard.writeText(markdown);
+    console.log('Markdown copied to clipboard!');
+} catch (error) {
+    console.error('Error copying Markdown:', error);
+}
+};
+
+const copyButton = document.querySelector(".cult-ad");
+copyButton.addEventListener('click', copyMarkdown);
